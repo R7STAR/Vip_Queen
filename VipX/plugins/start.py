7761ffa37a78fbec4378e9e4e209e54e124283c0
R7STAR <141929043+R7STAR@users.noreply.github.com>
2023-10-03 10:36:18 +0530

    Update start.py

--- VipX/plugins/start.py
+++ VipX/plugins/start.py
@@ -44,7 +44,7 @@ async def start_comm(client, message: Message, _):
         name = message.text.split(None, 1)[1]
         if name[0:4] == "help":
             keyboard = help_pannel(_)
-            await message.reply_sticker("CAACAgEAAxkBAAEIS2FkHfJQpbvRTfhyfS67BxulycCtegAC-gADUSkNORIJSVEUKRrhLwQ")
+            await message.reply_sticker("CAACAgUAAx0CcOKqzQACAdZlG54gtc5oI7z1PC5wzdiGxZuwvAACXQUAAqaSyFd9ZTsDEyTrmDAE")
             return await message.reply_photo(
                        photo=config.START_IMG_URL,
                        caption=_["help_1"].format(config.SUPPORT_HEHE), reply_markup=keyboard
